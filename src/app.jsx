441d96d0db506024c0a2b3fcb8cc768635e14bfb
Clayton Stallings <crstall2004@gmail.com>
2026-02-13 16:13:01 -0700

    got the app.jsx created

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+  <div class="body">
+    <header class="container-fluid">
+      <nav class="navbar fixed-top navbar-dark">
+      
+        <menu class="navbar-nav">
+          <h1 class="navbar-brand">LangLearn</h1>
+          <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
+          <li class="nav-item"><a class="nav-link" href="friends.html">Friends</a></li>
+          <li class="nav-item"><a class="nav-link" href="browse.html">Browse Decks</a></li>
+          <li class="nav-item"><a class="nav-link" href="study.html">Study</a></li>
+        </menu>
+      </nav>
+    </header>
+
+      <main>App components go here</main>
+
+    <footer class="d-flex justify-content-between align-items-center p-3">
+      <span class="text-reset">Clayton Stallings</span>
+      <a href="https://github.com/crstall4/mystartup" class="nav-link">GitHub</a>
+    </footer>
+    </div>
+  );
 }
