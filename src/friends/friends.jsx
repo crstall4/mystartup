51182ae53b7f8f2ed1e9627b61e6cc796a5f1206
Clayton Stallings <crstall2004@gmail.com>
2026-02-13 16:51:06 -0700

    porting more of the sites over

--- src/friends/friends.jsx
+++ src/friends/friends.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 export function Friends() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>friends displayed here</div>
+    <main className="container-fluid bg-light text-center text-dark d-flex flex-column justify-content-center">
+        {/* Webhook placeholder!!!! */}
+      <div>
+        <h1>Friends</h1>
+        <h2>Welcome, User!</h2>
+
+        <ul className="friends">
+          <li className="player-name">Friend 1: On Login Page</li>
+          <li className="player-name">Friend 2: Last online 3 minutes ago</li>
+          <li className="player-name">Friend 3: Reviewing Deck 1</li>
+        </ul>
+      </div>
     </main>
   );
 }
--- src/study/study.jsx
+++ src/study/study.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import '../app.css';
 
 export function Study() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>study displayed here</div>
+    <main className="container-fluid bg-light text-center text-dark d-flex flex-column justify-content-center">
+        <div className="study-card">
+            <svg width="150" height="100" viewBox="0 0 300 200" style={{border: "1px solid #000"}}>
+                <text x="150" y="100" fontSize="30" textAnchor="middle" dominantBaseline="middle">CARD</text>
+            </svg>
+        </div>
+        
+        <div
+          className="study-buttons"
+          style={{ border: "1px solid #000", padding: "10px", display: "inline-block" }}
+        >
+            <p>How well did you know this card?</p>
+            <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+            </div>
+        </div>
+
+        <div>
+            <p>Current time: 1:52pm</p>
+        </div>
+      
     </main>
   );
 }
