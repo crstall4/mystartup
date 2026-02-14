b9197eb86d9d53e1dc4038eca7442b3bef084872
Clayton Stallings <crstall2004@gmail.com>
2026-02-13 17:05:10 -0700

    fixed the browse page

--- src/browse/browse.jsx
+++ src/browse/browse.jsx
@@ -7,10 +7,10 @@ export function Browse() {
     <main className="container-fluid bg-light text-center text-dark d-flex flex-column justify-content-center">
       <div>
       <h1>Decks</h1>
-      <h3><NavLink to="study" className="deck">Spaced Review</NavLink></h3>
-        <h3><NavLink to="study" className="deck">Deck 1</NavLink></h3>
-        <h3><NavLink to="study" className="deck">Deck 2</NavLink></h3>
-        <h3><NavLink to="study" className="deck">Deck 3</NavLink></h3>
+      <h3><NavLink to="/study" className="deck">Spaced Review</NavLink></h3>
+        <h3><NavLink to="/study" className="deck">Deck 1</NavLink></h3>
+        <h3><NavLink to="/study" className="deck">Deck 2</NavLink></h3>
+        <h3><NavLink to="/study" className="deck">Deck 3</NavLink></h3>
       </div>
     </main>
   );
