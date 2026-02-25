35cb09a16d6d5b357d2da9ea3370f86675c886a7
Clayton Stallings <crstall2004@gmail.com>
2026-02-25 11:59:55 -0700

    added websocket simulation

--- src/friends/friends.jsx
+++ src/friends/friends.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 import '../app.css';
 
 export function Friends({user}) {
+  const [msg, setMsg] = React.useState('...listening');
+
+  React.useEffect(() => {
+    setInterval(() => {
+      const names = ['clayton', 'carter', 'michael', 'andrew', 'jeffery', 'jacob'];
+      const randomName = names[Math.floor(Math.random() * names.length)];
+      const randomCount = Math.floor(Math.random() * 100) + 1;
+      const newMsg = `${randomName}: ${randomCount}`;
+      setMsg(newMsg);
+    }, 1000);
+  })
+
   return (
     <main className="container-fluid bg-light text-center text-dark d-flex flex-column justify-content-center">
         {/* Webhook placeholder!!!! */}
+        <div>{msg}</div>
       <div>
         <h1>Friends</h1>
         <h2>Welcome, User!</h2>
