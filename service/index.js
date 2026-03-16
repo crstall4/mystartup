a035c78c28802542f0ad86c178ac995f026413e8
Clayton Stallings <crstall2004@gmail.com>
2026-03-16 12:19:04 -0600

    created a barebones backend that allows you to curl /api/health to see if the backend works. this technically checks the boxes of nodejs/express http service and backend service endpoints, but I wont fill that out on notes until I have something more than the bare minimum

--- service/index.js
+++ service/index.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.static('public'));
+
+app.get('/api/health', (req, res) => {
+res.send({ ok: true });
+});
+
+app.listen(port, () => {
+console.log('Backend listening on port ' + port);
+});
