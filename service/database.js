66a3536730189ebb88bd49d179c7950d2477d2eb
Clayton Stallings <crstall2004@gmail.com>
2026-03-18 19:14:38 -0600

    we are able to connect to my database now!

---
+++ service/database.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+
+const client = new MongoClient(url);
+const db = client.db('rental');
+const collection = db.collection('house');
+
+//simons test db code
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+async function main() {
+  try {
+    // add all the following database code here
+    
+  } finally {
+    client.close();
+  }
+}
+
+main();
