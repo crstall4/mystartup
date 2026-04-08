ddd09340f4b3a1418f7bc3d0f82eff3c5951b6c0
Clayton Stallings <crstall2004@gmail.com>
2026-04-07 18:46:59 -0600

    delete all the randomly generated placeholder stuff out of the friends page

--- src/friends/friends.jsx
+++ src/friends/friends.jsx
@@ -1,30 +1,8 @@
 import React from 'react';
 import '../app.css';
 
-export function Friends({user, friends, addFriend, removeFriend}) {
-  const statusesOptions = ["Offline", "On Friends Page", "On Login Page", "On Study Page", "On Browse Page"];
-  const [statuses, setStatuses] = React.useState({});
+export function Friends({user, friends, addFriend, removeFriend, statuses}) {
   const [friendName, setFriendName] = React.useState('');
-
-  React.useEffect(() => {
-    const initialStatuses = {};
-    friends.forEach(friend => {
-      initialStatuses[friend] = "Offline";
-    });
-    setStatuses(initialStatuses);
-
-    const interval = setInterval(() => {
-      setStatuses(prevStatuses => {
-        const newStatuses = { ...prevStatuses };
-        const randomFriend = friends[Math.floor(Math.random() * friends.length)];
-        const randomStatus = statusesOptions[Math.floor(Math.random() * statusesOptions.length)];
-        newStatuses[randomFriend] = randomStatus;
-        return newStatuses;
-      });
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [friends]);
 
   return (
     <main className="container-fluid bg-light text-center text-dark d-flex flex-column justify-content-center">
